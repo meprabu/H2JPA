05146950b2b9444001a53eaf939f255cdcb7d6e2
Prabu Jayapandian <Prabu.Jayapandian@dmh.ms.gov>
2017-02-21 17:25:19 -0600

    firstcommit
    Signed-off-by: Prabu Jayapandian <Prabu.Jayapandian@dmh.ms.gov>

---
+++ src/main/java/com/h2db/H2hibernateApplication.java
@@ -0,0 +1,59 @@
+package com.h2db;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@SpringBootApplication
+public class H2hibernateApplication {
+	
+	@Autowired
+    private Environment environment;
+	
+	
+
+	 @Bean
+	    public DataSource dataSource() {
+	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+	        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+	        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+	        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+	        return dataSource;
+	    }
+	 
+	 @Bean
+	    public LocalSessionFactoryBean sessionFactory() {
+	        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+	        sessionFactory.setDataSource(dataSource());
+	        sessionFactory.setPackagesToScan(new String[] { "com.prabu.repository" });
+	        sessionFactory.setHibernateProperties(hibernateProperties());
+	        return sessionFactory;
+	     }
+	 
+	 private Properties hibernateProperties() {
+	        Properties properties = new Properties();
+	        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+	        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+	        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+	        return properties;        
+	    }
+	 
+	 @Bean(initMethod="start",destroyMethod="stop")
+	 public org.h2.tools.Server h2WebConsonleServer () throws SQLException {
+	   return org.h2.tools.Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8082");
+	 }
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2hibernateApplication.class, args);
+	}
+}
